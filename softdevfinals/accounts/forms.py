f72fbffdec80d9b4a3cd6c1173cb595eb7979a58
Mujenyy <salungakelsey@gmail.com>
2024-10-03 16:35:31 +0800

    Register

--- softdevfinals/accounts/forms.py
+++ softdevfinals/accounts/forms.py
@@ -5,37 +5,69 @@ from .models import Profile
 
 
 class CustomLoginForm(forms.Form):
-    email = forms.EmailField(required=True)
-    password = forms.CharField(widget=forms.PasswordInput, required=True)
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your password'}),
+        required=True
+    )
 
-    
+
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)  # Adding the email field
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'})
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')  # Include email
+        fields = ('username', 'email', 'password1', 'password2')
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']  # Save email to the user object
-        if commit:
-            user.save()
-        return user
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter a username'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter a password'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirm your password'})
+
+        self.fields['password1'].widget.attrs.update({'class': 'form-control col-md-6'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control col-md-6'})
+
 
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'})
+    )
 
     class Meta:
         model = User
         fields = ['username', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your username'}),
+        }
+
+
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image', 'bio']
+        widgets = {
+            'bio': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter a short bio'}),
+        }
 
 
 class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100, required=True)
-    email = forms.EmailField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    subject = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter the subject'})
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'})
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter your message'}),
+        required=True
+    )
